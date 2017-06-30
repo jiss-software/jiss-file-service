d139686a7ab5735422e2210e7ae3031ee8a6b4db
Anton Iskov <a.iskov@oberthur.com>
2017-06-30 15:06:14 +0200

    Improve docker local run

--- docker.run.sh
+++ docker.run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-printf "\e[1mBUILDING jiss/file-service version ${1:-local}...\e[0m\n"
+CUR_NAME=file-service
+CUR_IMAGE=jiss/file-service:${1:-local}
 
-docker build -t jiss/file-service:${1:-local} . || {
+printf "\e[1mBUILDING ${CUR_IMAGE}...\e[0m\n"
+
+docker build -t "${CUR_IMAGE}" . || {
     printf "\e[31mDOCKER IMAGE BUILD FAIL!\e[0m\n" 1>&2
     exit 1;
 }
@@ -11,5 +14,5 @@ printf "\e[1mDOCKER IMAGE BUILD DONE.\e[0m\n"
 
 printf "\e[1mDOCKER STARTING...\e[0m\n"
 
-docker ps -a | grep file-service && docker rm -f file-service
-docker run -d --name file-service -p 33003:33003 -v $(pwd)/handler:/usr/lib/file-service/handler jiss/file-service:${1:-local}
+docker ps -a | grep "${CUR_NAME}" && docker rm -f "${CUR_NAME}"
+docker run -d --name "${CUR_NAME}" -p 33003:33003 -v $(pwd)/handler:/usr/lib/file-service/handler "${CUR_IMAGE}"
